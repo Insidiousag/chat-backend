fa2bd9ca73e335767dc07dcf764e1dc22f6f9732
Insidiousag <aman19022004@gmail.com>
2025-07-22 01:25:16 +0530

    Increase body size limit to 10MB

--- src/index.js
+++ src/index.js
@@ -78,8 +78,12 @@ app.use(
   })
 );
 
-app.use(express.json());
+//app.use(express.json());
 app.use(cookieParser());
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
 
 // 2) Your API routes
 app.use("/api/auth", authRoutes);
